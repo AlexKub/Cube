f81f79478d7874daddb5d400a8f7d06cf2e9d4c1
Kamerton <donkamerton@yandex.ru>
2019-08-15 22:38:46 +0300

    Добавил ILogReader

---
+++ Cube/Log/ILogMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Cube.Log
+{
+    /// <summary>
+    /// Собщение логирования
+    /// </summary>
+    /// <typeparam name="TKey">Тип ключа поиска сообщения</typeparam>
+    public interface ILogMessage<TKey> 
+    {
+        /// <summary>
+        /// Ключ поиска сообщения
+        /// </summary>
+        TKey MessageKey { get; }
+    }
+}
