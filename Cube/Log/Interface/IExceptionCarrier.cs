8212583ee57cdab4425fcb63c7ba33dfa53c0465
Kamerton <donkamerton@yandex.ru>
2019-08-18 00:09:21 +0300

    Добавил "носителя" сообщений

---
+++ Cube/Log/Interface/IExceptionCarrier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cube.Log
+{
+    /// <summary>
+    /// Сущность с экземпляром исключения
+    /// </summary>
+    public interface IExceptionCarrier
+    {
+        /// <summary>
+        /// Флаг наличия исключения
+        /// </summary>
+        bool HasException { get; }
+
+        /// <summary>
+        /// Экзмемпляр исключения
+        /// </summary>
+        Exception Exception { get; }
+    }
+}
