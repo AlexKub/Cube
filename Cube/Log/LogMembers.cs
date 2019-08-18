819c2dee01a22658ce9f549dd2ba817fe57f8817
Kamerton <donkamerton@yandex.ru>
2019-08-19 00:38:53 +0300

    Добавил флаги логируемых членов

---
+++ Cube/Log/LogMembers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cube.Log
+{
+    /// <summary>
+    /// Логируемые члены класса
+    /// </summary>
+    [Flags]
+    public enum LogMembers
+    {
+        /// <summary>
+        /// Не определено (по умолчанию)
+        /// </summary>
+        UnKnown = 0,
+        /// <summary>
+        /// Свойства
+        /// </summary>
+        Properties = 1,
+        /// <summary>
+        /// Поля
+        /// </summary>
+        Fields = 1 << 1
+    }
+}
