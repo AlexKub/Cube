caa9ebd026daff98f37b9fe5d8f814ec00f683c1
Kamerton <donkamerton@yandex.ru>
2019-08-19 00:40:29 +0300

    Наброски логирования экземпляров

---
+++ Cube/Log/TypeStringMessageBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube.Log
+{
+    /// <summary>
+    /// Построение строкового сообщения для типа
+    /// </summary>
+    public abstract class TypeStringMessageBuilder<T> : IMeassageBuilder<string, T>
+    {
+        public abstract string Build(T data);
+    }
+}
