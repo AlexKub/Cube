d95823536e2b88cf39516f67d72a1425d93980cf
Kamerton <donkamerton@yandex.ru>
2019-08-19 00:40:10 +0300

    Аттрибут логирования типа

---
+++ Cube/Log/LogMembersAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cube.Log
+{
+    /// <summary>
+    /// Логируемые члены класса
+    /// </summary>
+    [System.Diagnostics.DebuggerDisplay("{DebugDisplay()}")]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class LogMembersAttribute : Attribute
+    {
+        /// <summary>
+        /// Логируемые члены класса
+        /// </summary>
+        public LogMembers LogMembers { get; private set; }
+        /// <summary>
+        /// Флаги поиска членов
+        /// </summary>
+        public BindingFlags BindingFlags { get; private set; }
+
+        /// <summary>
+        /// Логируемые члены класса
+        /// </summary>
+        /// <param name="logMembers">Флаги членов</param>
+        /// <param name="bindingFlags">Флаги поиска</param>
+        public LogMembersAttribute(LogMembers logMembers, BindingFlags bindingFlags)
+        {
+            LogMembers = logMembers;
+            BindingFlags = bindingFlags;
+        }
+
+        string DebugDisplay() => $"Members: {(LogMembers.ToString())} -- Flags: {(BindingFlags.ToString())}";
+    }
+}
---
+++ Cube/Log/LogMembersInheritedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cube.Log
+{
+    /// <summary>
+    /// Атрибут логирования членов текущего класса и его наследников
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, Inherited = true)]
+    public class LogMembersInheritedAttribute : LogMembersAttribute
+    {
+        /// <summary>
+        /// Атрибут логирования членов текущего класса и его наследников
+        /// </summary>
+        public LogMembersInheritedAttribute(LogMembers logMembers, BindingFlags bindingFlags) : base(logMembers, bindingFlags)
+        {
+        }
+    }
+}
