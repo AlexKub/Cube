a2d183fef483133642c637d7282694c559717081
Kamerton <donkamerton@yandex.ru>
2019-08-19 00:39:46 +0300

    Аттрибут пропуска логирования

---
+++ Cube/Log/SkipLogingAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Cube.Log
+{
+    /// <summary>
+    /// Флаг пропуска логирования для данного элемента
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property)]
+    public class SkipLogingAttribute : Attribute { }
+}
